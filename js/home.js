8f924c78adf31e6999f8468bebec022e093e9104
Andres Condo <1dkevinandres.b@gmail.com>
2020-03-04 12:00:23 -0500

    agregando el templates para el API

--- js/home.js
+++ js/home.js
@@ -1,3 +1,5 @@
+//Comillas invertidas (  `  )   
+
 const www = 'https://yts.mx/api/v2';
 console.log('Hola mundo');
 const getUser = new Promise(function(todoBien, todoMal){
@@ -53,23 +55,42 @@ fetch('https://randomuser.me/api/')
 	const actionList = await getData('https://yts.mx/api/v2/list_movies.json?genre=action');
 	const dramaList = await getData('https://yts.mx/api/v2/list_movies.json?genre=drama');
 	const terrorList = await getData('https://yts.mx/api/v2/list_movies.json?genre=terror');
-	
 	console.log('actionList: ',actionList,dramaList,terrorList);
+	function videoItemTemplate(movie){
+		return (
+		`<div class="primaryPlaylist-list" id="drama">
+			<div class="primaryPlaylistItem">
+			<div class="primaryPlaylistItem-image">
+				<img src="${movie.medium_cover_image}">
+			</div>
+			<h4 class="primaryPlaylistItem-title">
+				${movie.title}
+			</h4>
+		</div>`
+		)
+	}
+	//IMPORTANTE cuando se usa una API sirve ver la ruta completa para la solicitud del dato requerido
+	actionList.data.movies.forEach((movie)=> {
+		const HTMLString = videoItemTemplate(movie);
+		console.log(HTMLString);
 
-	const $actionContainer = document.getElementByTd('#action');
-	const $dramaContainer = document.getElementByTd('#drama');
-	const $animationContainer = document.getElementByTd('#animation');
-	const $featuringContainer = document.getElementByTd('#featuring');
-	const $form = document.getElementByTd('#form');
-	const $home = document.getElementByTd('#home');
+	})
 
+	const $actionContainer = document.getElementById('#action');
+	const $dramaContainer = document.getElementById('#drama');
+	const $animationContainer = document.getElementById('#animation');
+	const $featuringContainer = document.getElementById('#featuring');
+	const $form = document.getElementById('#form');
+	const $home = document.getElementById('#home');
 
-
-	const $modal = document.querySelector('modal');
-	const $overlay = document.getElementByTd('overlay');
-	const $hideModal = document.getElementByTd('hide-modal');
+	const $modal = document.getElementById('modal');//usar el getEBI, para luego usar el querySelector
+	const $overlay = document.getElementById('overlay');
+	const $hideModal = document.getElementById('hide-modal');
 	
 	const $modalTitle = $modal.querySelector('h1');
 	const $modalImage = $modal.querySelector('img');
 	const $modalDescription = $modal.querySelector('p');
+
+	
+	// console.log(videoItemTemplate('image/covers/midnight.jpg', 'Bitcoins'));
 })()
