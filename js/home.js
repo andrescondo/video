30643cd5c9bab35b5253ef4a69f9a84e5cb22ed3
Andres Condo <1dkevinandres.b@gmail.com>
2020-03-07 20:11:33 -0500

    reutilizando funciones para mayor legibilidad

--- js/home.js
+++ js/home.js
@@ -54,31 +54,46 @@ fetch('https://randomuser.me/api/')
 	}
 	const actionList = await getData('https://yts.mx/api/v2/list_movies.json?genre=action');
 	const dramaList = await getData('https://yts.mx/api/v2/list_movies.json?genre=drama');
-	const terrorList = await getData('https://yts.mx/api/v2/list_movies.json?genre=terror');
-	console.log('actionList: ',actionList,dramaList,terrorList);
-	function videoItemTemplate(movie){
+	const animationList = await getData('https://yts.mx/api/v2/list_movies.json?genre=animation');
+	console.log(actionList,dramaList,animationList);
+	function videoItemTemplate(movie)
+	{
 		return (
-		`<div class="primaryPlaylist-list" id="drama">
-			<div class="primaryPlaylistItem">
+		`<div class="primaryPlaylistItem">
 			<div class="primaryPlaylistItem-image">
 				<img src="${movie.medium_cover_image}">
 			</div>
 			<h4 class="primaryPlaylistItem-title">
 				${movie.title}
-			</h4>
-		</div>`
+			</h4>`
 		)
 	}
+	function createTemplate(HTMLString)
+	{
+		const html = document.implementation.createHTMLDocument();
+		html.body.innerHTML = HTMLString;
+		return html.body.children[0];
+	}
+
+	function renderMovieList(list, $container)
+	{
+		$container.children[0].remove();
+		list.forEach((movie)=> {
+			const HTMLString = videoItemTemplate(movie);
+			const movieElement = createTemplate(HTMLString);
+			$actionContainer.append(movieElement);
+		})
+	}
 	//IMPORTANTE cuando se usa una API sirve ver la ruta completa para la solicitud del dato requerido
-	actionList.data.movies.forEach((movie)=> {
-		const HTMLString = videoItemTemplate(movie);
-		console.log(HTMLString);
+	const $actionContainer = document.getElementById('action');//cuando se usa 	gEBI, no se debe llamar con un #
+	renderMovieList(actionList.data.movies , $actionContainer);
 
-	})
+	const $dramaContainer = document.getElementById('drama');
+	renderMovieList(dramaList.data.movies , $dramaContainer);
 
-	const $actionContainer = document.getElementById('#action');
-	const $dramaContainer = document.getElementById('#drama');
-	const $animationContainer = document.getElementById('#animation');
+	const $animationContainer = document.getElementById('animation');
+	renderMovieList(animationList.data.movies , $animationContainer);
+
 	const $featuringContainer = document.getElementById('#featuring');
 	const $form = document.getElementById('#form');
 	const $home = document.getElementById('#home');
